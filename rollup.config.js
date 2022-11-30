0cfa783726eae7d1d6b5ac5db8441ad3e6193bf3
atikur-rabbi <msawon94@gmail.com>
2022-11-30 14:14:33 +0600

    1 file modified, 8 added

---
+++ rollup.config.js
@@ -0,0 +1,23 @@
+import lwc from '@lwc/rollup-plugin';
+import replace from '@rollup/plugin-replace';
+
+const isProduction = process.env.production;
+
+export default {
+  input: 'src/main.js',
+
+  output: {
+    file: 'dist/main.js',
+    format: 'esm',
+  },
+
+  plugins: [
+    replace({
+      'process.env.NODE_ENV': JSON.stringify(
+        isProduction ? 'production' : 'development'
+      ),
+      preventAssignment: true,
+    }),
+    lwc(),
+  ],
+};
---
+++ src/main.js
@@ -0,0 +1,3 @@
+import Header from 'c/header';
+
+customElements.define('c-header', Header.CustomElementConstructor);

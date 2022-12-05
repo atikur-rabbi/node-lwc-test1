959f10bb012630b89f3848db3a27e0f2695660a2
atikur-rabbi <msawon94@gmail.com>
2022-12-05 12:06:47 +0600

    3 files modified, 3 added

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
 import Header from 'c/header';
+import Assets from 'c/at_assets';
 
 customElements.define('c-header', Header.CustomElementConstructor);
+customElements.define('c-at_assets', Assets.CustomElementConstructor);
